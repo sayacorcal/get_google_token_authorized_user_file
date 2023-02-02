b1a48c7219031526dfa961e11fddc8e5cfbca3ff
sayacorcal <sayacorcal@gmail.com>
2023-02-02 15:33:34 -0500

    update auth code

--- get_google_token_authorized_user_file.py
+++ get_google_token_authorized_user_file.py
@@ -23,16 +23,26 @@ print("Scopes: "    ,   SCOPES)
 
 creds = None
 
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+
 if os.path.exists(token_file):
-  creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+    creds = Credentials.from_authorized_user_file(token_file, SCOPES)
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
   if creds and creds.expired and creds.refresh_token:
     creds.refresh(Request())
   else:
-    flow = InstalledAppFlow.from_client_secrets_file( credentials_file , scopes=SCOPES ) # 'credentials.json', SCOPES)
-    #flow.user_agent = APPLICATION_NAME
+    flow = InstalledAppFlow.from_client_secrets_file( CLIENT_SECRET , scopes=SCOPES ) # 'credentials.json', SCOPES)
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+    
     creds = flow.run_local_server(port=0)
   # Save the credentials for the next run
   with open(token_file, 'w') as token:
-    token.write(creds.to_json())
+      token.write(creds.to_json())
